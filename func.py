cba1e7b046b1c29247038cbe525c3f6e10df3efd
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2019-10-07 10:02:15 +0530

    Added func.py
    The function lab_contrast() applies CLAHE on RGB images for color correction.

---
+++ func.py
@@ -0,0 +1,13 @@
+
+import cv2
+import sys
+import numpy as np
+
+def lab_contrast(img, grid_size=7):
+	img_lab = cv2.cvtColor(img, cv.COLOR_BGR2LAB)
+	lab_planes = cv2.split(img_lab)
+	clh = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(grid_size,grid_size))
+	lab_planes[0] = clh.apply(lab_planes[0])
+	img_lab = cv2.merge(lab_planes)
+	img_bgr = cv2.cvtColor(img_lab, cv.COLOR_LAB2BGR)
+	return img_bgr
