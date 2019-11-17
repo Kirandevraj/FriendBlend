de78030c301886a8d40913ce75793988e28bc8f6
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2019-11-17 11:57:37 +0530

    Update func.py

--- func.py
+++ func.py
@@ -89,3 +89,159 @@ def transform_points(pt1, homography_matrix):
     new_points[new_points <0] = 0
     new_points = new_points.reshape((new_points.shape[0], new_points.shape[2]))
     return new_points
+
+def keypoints(img):
+	img_corrected = lab_contrast(img,7)
+	fast = cv2.FastFeatureDetector_create()
+	keypoints = fast.detect(img_corrected.copy())
+	kp = []
+	for i in range(len(keypoints)):
+		kp.append([keypoints[i].pt[0], keypoints[i].pt[1]])
+	
+	return kp
+
+def keypoints_orb_detector(img, n=1000):
+	orb = cv.ORB_create(nfeatures=n)
+	k = orb.detect(img)
+	return k
+def L2_distance(pt1, pt2):
+	return np.linalg.norm(pt1-pt2)
+
+def list2pt(keypoints):
+	kp = []
+	for i in range(len(keypoints)):
+		kp.append([keypoints[i].pt[0], keypoints[i].pt[1]])
+	kp = np.asarray(kp)
+	return kp
+
+def draw_keypoints(img, kp):
+	for i in range(len(kp)):
+		 cv2.circle(img,(int(kp[i][0]), int(kp[i][1])), 2, (0,0,255), -1)
+	return img
+
+def sort_order(img1,img2,body1,body2):
+    col_start = body1[0][2]
+    col_end= body2[0][0]
+    if col_start>col_end:
+        img_temp = img1.copy()
+        img1=img2.copy()
+        img2 = img_temp.copy()
+        body_temp =body1.copy()
+        body1 = body2.copy()
+        body2 = body_temp.copy()
+    return img1,img2,body1,body2
+
+def blend_or_cut(body1,body2, n=500):
+    if body2[0][2]-body1[0][2]<n:
+        print("Bodies close, Implementing GrabCut")
+        return "grabcut"
+    else:
+        print("Bodies far enough, Implementing Alpha Blending")
+        return "blend"
+
+def grabcut(img1,img2,body1,body2):
+
+    a1,b1,c1,d1,w1,h1 = body1[0]
+    a2,b2,c2,d2,w2,h2 = body2[0]
+    
+    mask = np.zeros(img1.shape[:2],np.uint8)
+    backgroundModel = np.zeros((1,65),np.float64)
+    foregroundModel = np.zeros((1,65),np.float64)
+    if w1*h1 > w2*h2:
+        foreground = img1.copy()
+        background = img2.copy()
+        rect = (a1,b1,c1,d1)
+        for i in range(a1,c1):
+            for j in range(b1,d1):
+                mask[j,i] = cv2.GC_PR_FGD
+        for i in range(a1+w1-5,c1-w1+5):
+            for j in range(b1+h1-5,b1+int(1*h1)+5):
+                mask[j,i] = cv2.GC_FGD
+        cv2.grabCut(foreground, mask,rect,backgroundModel,foregroundModel,1,cv2.GC_INIT_WITH_MASK)
+        mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+        img = foreground*mask2[:,:,np.newaxis]
+
+        return img,background
+    else:
+        foreground = img2.copy()
+        background = img1.copy()
+        rect = (a2,b2,c2,d2)
+        for i in range(a2,c2):
+            for j in range(b2,d2):
+                mask[j,i] = cv2.GC_PR_FGD
+        for i in range(a2+w2-5,c2-w2+5):
+            for j in range(b2+h2-5,b2+int(1*h2)+5):
+                mask[j,i] = cv2.GC_FGD
+        cv2.grabCut(foreground, mask,rect,backgroundModel,foregroundModel,1,cv2.GC_INIT_WITH_MASK)
+        mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+        img = foreground*mask2[:,:,np.newaxis]
+
+        return img,background
+
+def alpha_blend(img1, img2, body1, body2):
+    op = np.zeros(img1.shape)
+    col_start = body1[0][2]
+    col_end= body2[0][0]
+    if col_start>col_end:
+        img_temp = img1.copy()
+        img1=img2.copy()
+        img2 = img_temp.copy()
+        body_temp =body1.copy()
+        body1 = body2.copy()
+        body2 = body_temp.copy()
+        op = np.zeros(img1.shape)
+        col_start = body1[0][2]
+        col_end = body2[0][0]
+    
+    step_size = 1/(col_end-col_start)
+    for x in range(col_start,col_end):
+        step_count = x-col_start
+        op[:,x,0] = ((1-(step_count*step_size))*img1[:,x,0])+((step_count*step_size)*img2[:,x,0])
+        op[:,x,1] = ((1-(step_count*step_size))*img1[:,x,1])+((step_count*step_size)*img2[:,x,1])
+        op[:,x,2] = ((1-(step_count*step_size))*img1[:,x,2])+((step_count*step_size)*img2[:,x,2])
+    op[:,0:col_start,:] = img1[:,0:col_start,:]
+    op[:,col_end:,:] = img2[:,col_end:,:]
+    return op
+
+def crop_image(img, H):
+	rows, cols, channels = img.shape
+	pts = np.float32([[0, 0], [cols, 0], [cols, rows], [0, rows]]).reshape(-1,
+		                                                           1, 2)
+	warp_points = cv2.perspectiveTransform(pts, H)
+	top_row = int(max(max(warp_points[0][0][1], warp_points[1][0][1]), 0))
+	bottom_row = int(min(min(warp_points[2][0][1], warp_points[3][0][1]), rows))
+
+	return img[top_row:bottom_row, 0:cols]
+
+def blend_cropped_image(background_img, input_img):
+    height, width, channels = input_img.shape
+    cropped_img_binary = np.zeros((height, width, 1), np.uint8)
+    cropped_img_gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+    cv2.threshold(cropped_img_gray, 0, 255, cv2.THRESH_BINARY, cropped_img_binary)
+    concat_image = np.concatenate((cropped_img_gray, cropped_img_binary[:,:,0]),axis=1)
+    merged_img = np.uint8((255 - cropped_img_binary) / 255) * background_img + input_img
+    element_sizes = [(5, 0)]
+    outer_mask = cropped_img_binary
+    for erosion_size, fore_coeff in element_sizes:
+        element_size = (erosion_size, erosion_size)
+        element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, element_size)
+        inner_mask = np.zeros((height, width, 1), np.uint8)
+        cv2.erode(outer_mask, element, dst=inner_mask)
+        mask = outer_mask - inner_mask
+        blended_section = np.uint8(fore_coeff * mask / 255 * input_img +(1 - fore_coeff) * mask / 255 * background_img)
+        inverse_mask = 255 - mask
+        inverse_mask = inverse_mask / 255  # convert to 1s and 0s
+        merged_img = inverse_mask * merged_img + blended_section
+        outer_mask = inner_mask
+    rows, cols, channels = inner_mask.shape
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if inner_mask[i, j] == np.uint8(255):
+                bottom_row = i
+                break
+    merged_img = merged_img[0:bottom_row, 0:cols]
+    return merged_img
+
+def blur_img(img):
+	blur = cv2.GaussianBlur(img,(3,3),0)
+	return blur
